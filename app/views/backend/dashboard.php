4a041c4bf936a8d2028e7aad72d83de66518e20d
tepchiva <tepchiva018@gmail.com>
2021-03-23 01:03:24 +0700

    update Header (breadcrumb)

--- app/views/backend/dashboard.php
+++ app/views/backend/dashboard.php
@@ -2,20 +2,20 @@
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header, breadcrumb) -->
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1>Title of each pages</h1>
           </div>
-          <!-- <div class="col-sm-6">
+          <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item"><a href="#">Layout</a></li>
               <li class="breadcrumb-item active">Fixed Layout</li>
             </ol>
-          </div> -->
+          </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
